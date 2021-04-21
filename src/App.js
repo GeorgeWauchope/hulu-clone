0a8d3ea11fb382c0ff88a72ad89ca9216d58eae8
George Wauchope <69352286+GeorgeWauchope@users.noreply.github.com>
2021-04-21 14:02:23 -0500

    Header.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build a Hulu clone!!!</h1>
+      <Header />
+
+      {/* Nav */}
+
+      {/* Results */}
     </div>
   );
 }
