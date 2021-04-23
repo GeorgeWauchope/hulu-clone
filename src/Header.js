fb62518817339131e05a5bca3046b73836c72832
George Wauchope <69352286+GeorgeWauchope@users.noreply.github.com>
2021-04-23 15:30:18 -0500

    Header.js component finished

--- src/Header.js
+++ src/Header.js
@@ -11,12 +11,35 @@ function Header() {
   return (
     <div className="header">
       <div className="header_icons">
-        <HomeIcon />
-        <FlashOnIcon />
-        <LiveTvIcon />
-        <VideoLibraryIcon />
-        <SearchIcon />
-        <PersonOutlineIcon />
+        <div className="header_icon header_icon--active">
+          <HomeIcon />
+          <p>Home</p>
+        </div>
+
+        <div className="header_icon">
+          <FlashOnIcon />
+          <p>Trending</p>
+        </div>
+
+        <div className="header_icon">
+          <LiveTvIcon />
+          <p>Verified</p>
+        </div>
+
+        <div className="header_icon">
+          <VideoLibraryIcon />
+          <p>Collections</p>
+        </div>
+
+        <div className="header_icon">
+          <SearchIcon />
+          <p>Search</p>
+        </div>
+
+        <div className="header_icon">
+          <PersonOutlineIcon />
+          <p>Account</p>
+        </div>
       </div>
       <img
         src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png"
